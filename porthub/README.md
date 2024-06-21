471328fb1e77eff7d24c3a52a6c323520da4c652
nathanbvn <94560509+nathanbvn@users.noreply.github.com>
2024-06-21 09:59:51 +0200

    Add files via upload

---
+++ porthub/README.md
@@ -0,0 +1,33 @@
+<p align="center"><img src="https://github.com/0adri3n/porthub/assets/62818208/747d8c7c-3958-44a9-ad13-0b1d5d2e68d1" width=300></p>
+
+<h1 align="center" id="title">porthub</h1>
+
+<p id="description">Secure &amp; Temporary chatroom</p>
+
+  
+  
+<h2>🧐 Features</h2>
+
+Here're some of the project's best features:
+
+*   Open a secure websocket to chat
+*   Use a local Electron app to chat on the websockets
+*   Delete websockets
+*   No logs no trace.
+
+  
+  
+<h2>💻 Built with</h2>
+
+Technologies used in the project:
+
+*   Python
+*   Flask
+*   Electron
+*   NoSQL
+*   JavaScript
+*   Websocket
+
+<h2>🛡️ License:</h2>
+
+This project is licensed under the MIT
---
+++ porthub/websocket_test/server.py
@@ -0,0 +1,24 @@
+import asyncio
+import websockets
+from websockets import WebSocketServerProtocol
+from typing import Set
+connected_clients: Set[WebSocketServerProtocol] = set()
+
+async def register_client(websocket: WebSocketServerProtocol):
+    connected_clients.add(websocket)
+    try:
+        async for message in websocket:
+            await broadcast(message)
+    finally:
+        connected_clients.remove(websocket)
+
+async def broadcast(message: str):
+    if connected_clients:
+        await asyncio.wait([asyncio.create_task(client.send(message)) for client in connected_clients])
+
+async def main():
+    async with websockets.serve(register_client, "localhost", 5665):
+        await asyncio.Future()  # Run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
