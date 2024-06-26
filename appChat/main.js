ca11e358caf1e976ab706af16e42c85d147902bf
Amna <94560621+blhmna@users.noreply.github.com>
2024-06-26 10:31:17 +0200

    Update main.js

--- appChat/main.js
+++ appChat/main.js
@@ -18,6 +18,7 @@ function createWindow() {
       contextIsolation: true,
       nodeIntegration: false,
     },
+    icon: path.join(__dirname, "assets/icon.png"),
   });
 
   win.loadURL(url.format({
