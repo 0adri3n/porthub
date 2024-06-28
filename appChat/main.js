42752c4c341a4a8d038936897468001eaef3ce87
Amna <94560621+blhmna@users.noreply.github.com>
2024-06-28 10:23:52 +0200

    final appChat ! (ssl/tls ?)

--- appChat/main.js
+++ appChat/main.js
@@ -1,8 +1,8 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-const url = require('url');
-const WebSocket = require('ws');
-const axios = require('axios');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const url = require("url");
+const WebSocket = require("ws");
+const axios = require("axios");
 
 let win;
 let ws;
@@ -14,39 +14,41 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
       contextIsolation: true,
       nodeIntegration: false,
     },
     icon: path.join(__dirname, "assets/icon.png"),
+    autoHideMenuBar: true,
   });
 
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'joinchat.html'),
-    protocol: 'file',
-    slashes: true,
-  }));
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "joinchat.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
 
-  win.on('closed', () => {
+  win.on("closed", () => {
     win = null;
   });
 }
 
-
-ipcMain.handle('check-credentials', async (event, token, username) => {
+ipcMain.handle("check-credentials", async (event, token, username) => {
   try {
-    const response = await axios.get('http://10.101.0.131:5000/token/' + token);
+    const response = await axios.get("http://127.0.0.1:5000/token/" + token);
     console.log(response.data.exists);
 
     if (response.data.exists === true) {
-      console.log('OK');
+      console.log("OK");
       const decoded = atob(token);
       const jsonObject = JSON.parse(decoded);
       port = jsonObject.port;
       pseudo = username;
       console.log(username);
 
-    //   ws = new WebSocket('ws://10.101.0.131:' + port);
+      //   ws = new WebSocket('ws://10.101.0.131:' + port);
       console.log(port);
 
       return true;
@@ -73,7 +75,7 @@ ipcMain.handle('check-credentials', async (event, token, username) => {
 //           pseudo = username;
 //           console.log(username);
 //           ws = new WebSocket('ws://localhost:'+port);
-//           console.log(port); 
+//           console.log(port);
 //           return true;
 //         }
 //         else{
@@ -89,37 +91,36 @@ ipcMain.handle('check-credentials', async (event, token, username) => {
 // });
 
 // Load second page on valid credentials
-ipcMain.on('load-second-page', (event, data) => {
+ipcMain.on("load-second-page", (event, data) => {
   if (win) {
     const filePath = path.join(__dirname, `client.html`);
-    win.loadURL(url.format({
-      pathname: filePath,
-      protocol: 'file',
-      slashes: true,
-    }));
+    win.loadURL(
+      url.format({
+        pathname: filePath,
+        protocol: "file",
+        slashes: true,
+      })
+    );
   }
 });
 
-
-ipcMain.on('get-port', (event) => {
+ipcMain.on("get-port", (event) => {
   event.returnValue = port;
 });
 
-ipcMain.on('get-username', (event) => {
+ipcMain.on("get-username", (event) => {
   event.returnValue = pseudo;
 });
 
+app.on("ready", createWindow);
 
-
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
